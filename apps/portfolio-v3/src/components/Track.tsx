5a8b2a8a003d5275352c2345d756f90ffaba5ee3
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-04-05 07:38:35 -0700

    Change: Tracks Compent Styles

--- apps/portfolio-v3/src/components/Track.tsx
+++ apps/portfolio-v3/src/components/Track.tsx
@@ -7,12 +7,13 @@ const Track = (track: Song) => {
       <p className='text-sm font-semibold text-white-600 -mt-[3px]'>
         {track.ranking}
       </p>
-      <div className='pl-3 flex'>
-        <div>
+      <div className='pl-3 flex mb-4 items-center'>
+        <div className='w-full max-w-14 max-h-14 h-full'>
           <Image
             src={track?.albumArt}
             width={50}
             height={50}
+            objectFit='cover'
             className='rounded-md'
             alt='NowPlaying Cove Image'
           ></Image>
@@ -22,11 +23,11 @@ const Track = (track: Song) => {
             href={track?.songUrl}
             target='_blank'
             rel='noopener noreferrer'
-            className='font-medium text-white-800 w-60 sm:w-96 md:w-full'
+            className='font-medium text-white-800 w-60 sm:w-96 md:w-full truncate'
           >
             {track?.title}
           </a>
-          <p className='text-white-600 mb-4 w-60 sm:w-96 md:w-full'>
+          <p className='text-white-600  w-60 sm:w-96 md:w-full truncate'>
             {track?.artist}
           </p>
         </div>
