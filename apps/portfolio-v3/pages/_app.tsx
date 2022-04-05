9b093c1172665efd1d7870d163144c042cf719b4
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-04-05 11:51:38 -0700

    Change: Changed analytics load strategy

--- apps/portfolio-v3/pages/_app.tsx
+++ apps/portfolio-v3/pages/_app.tsx
@@ -13,7 +13,7 @@ const MyApp = ({ Component, pageProps }) => {
       <Script
         src='https://analytics.bachitterch.com/umami.js'
         data-website-id='f02c92de-db59-42c6-8459-a1b6fdfe1c17'
-        strategy='lazyOnload'
+        strategy='afterInteractive'
       />
       <script
         type='text/partytown'
