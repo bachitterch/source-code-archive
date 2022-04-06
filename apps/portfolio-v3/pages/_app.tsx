10c69fa1f3d8c01c61bb2cd976bae5107064e948
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-04-05 18:49:32 -0700

    Change: Removed Partytown and Google Analytics

--- apps/portfolio-v3/pages/_app.tsx
+++ apps/portfolio-v3/pages/_app.tsx
@@ -7,27 +7,9 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <>
       <Script
-        strategy='worker'
-        src='https://www.googletagmanager.com/gtag/js?id=G-7WHCVMKCS8'
-      />
-      <Script
         src='https://analytics.bachitterch.com/umami.js'
         data-website-id='f02c92de-db59-42c6-8459-a1b6fdfe1c17'
         strategy='afterInteractive'
-      />
-      <script
-        type='text/partytown'
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            window.gtag = function gtag(){window.dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-7WHCVMKCS8', { 
-                page_path: window.location.pathname,
-            });
-        `
-        }}
       />
       <DefaultSeo {...seo} />
       <Component {...pageProps} />
