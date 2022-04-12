07c1d328ca5c202912ef06f82ee38f507635a809
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-04-12 06:54:22 -0700

    Change: Added show active and labels to nav-items

---
+++ apps/portfolio-v3/src/components/Navbar.tsx
@@ -0,0 +1,68 @@
+import { motion, AnimateSharedLayout } from 'framer-motion'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import Link from 'next/link'
+import cn from 'classnames'
+import {
+  Home,
+  User,
+  Briefcase,
+  Edit2,
+  Aperture,
+  Activity,
+  Tool
+} from 'react-feather'
+
+function NavItem({ href, name, children }) {
+  const router = useRouter()
+  const isActive = router.asPath === href
+  return (
+    <Link href={href}>
+      <a
+        className={cn(
+          isActive ? 'bg-white-0' : 'bg-white-200',
+          'relative group block hover:bg-white-10 rounded-xl transition-all duration-300'
+        )}
+      >
+        <span>{children}</span>
+        <span className='absolute -inset-x-6 mx-auto bg-white-800 -top-12 py-1 px-2 text-center scale-0 sm:group-hover:scale-100 text-sm rounded-md transition-all duration-300 max-w-max text-white-50'>
+          {name}
+        </span>
+      </a>
+    </Link>
+  )
+}
+
+const Nav = () => {
+  return (
+    <footer className='fixed inset-x-0 bottom-16 z-[9999999999] w-full text-center transition-all'>
+      <nav className='mx-auto max-w-fit'>
+        <div className='flex mx-8 items-center space-x-2 rounded-[1.5rem] border-2 border-white-300 p-3 transition-all backdrop-blur bg-white-50/80 fixed_nav  overflow-x-auto sm:overflow-x-visible'>
+          <NavItem href='/' name='Home'>
+            <Home className='p-3 text-white-800' size={40} />
+          </NavItem>
+          <NavItem href='/now' name='Now'>
+            <Activity className='p-3 text-white-800' size={40} />
+          </NavItem>
+          <NavItem href='/about' name='About'>
+            <User className='p-3 text-white-800' size={40} />
+          </NavItem>
+          <NavItem href='/projects' name='Projects'>
+            <Briefcase className='p-3 text-white-800' size={40} />
+          </NavItem>
+          <NavItem href='/blog' name='Blog'>
+            <Edit2 className='p-3 text-white-800' size={40} />
+          </NavItem>
+          <NavItem href='/play' name='Play'>
+            <Aperture className='p-3 text-white-800' size={40} />
+          </NavItem>
+          <NavItem href='/colophon' name='Colophon'>
+            <Tool className='p-3 text-white-800' size={40} />
+          </NavItem>
+        </div>
+      </nav>
+    </footer>
+  )
+}
+
+export default Nav
--- apps/portfolio-v3/src/layouts/__layout.tsx
+++ apps/portfolio-v3/src/layouts/__layout.tsx
@@ -1,12 +1,12 @@
-import NavBar from '@components/NavBar'
+import Navbar from '@components/Navbar'
 
 const Layout = (props, init) => {
   const { children } = props
   return (
     <>
-      <NavBar />
       <main className='mx-auto flex w-full max-w-2xl flex-col justify-center px-8 py-36'>
         {children}
+        <Navbar />
       </main>
     </>
   )
