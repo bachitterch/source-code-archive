aa9b02b08f8ecfddd370b40ec5eba9a0a04d6bb7
Bachitter Chahal <officialbachitterchahal@gmail.com>
2022-04-12 09:39:32 -0700

    Change: Removed Framer-Motion

--- apps/portfolio-v3/src/layouts/__layout.tsx
+++ apps/portfolio-v3/src/layouts/__layout.tsx
@@ -1,26 +1,12 @@
-import { AnimatePresence, motion } from 'framer-motion'
 import Navbar from '@components/Navbar'
 
 const Layout = (props, init) => {
   const { children } = props
   return (
     <>
-      <AnimatePresence>
-        <motion.main
-          initial={{ opacity: 0, y: 8 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-            transition: {
-              duration: 0.4,
-              ease: [0.11, 0, 0.5, 0]
-            }
-          }}
-          className='mx-auto flex w-full max-w-2xl flex-col justify-center px-8 py-36'
-        >
-          {children}
-        </motion.main>
-      </AnimatePresence>
+      <main className='mx-auto flex w-full max-w-2xl flex-col justify-center px-8 py-36'>
+        {children}
+      </main>
       <Navbar />
     </>
   )
